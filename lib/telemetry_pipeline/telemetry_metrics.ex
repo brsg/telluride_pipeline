9585131b38824ad76327ae4d2f2c5562bdc54048
Alan Strait <alan@brsg.biz>
2021-02-14 19:54:47 -0700

    Removed unused functions.

--- lib/telemetry_pipeline/telemetry_metrics.ex
+++ lib/telemetry_pipeline/telemetry_metrics.ex
@@ -94,32 +94,6 @@ defmodule TelemetryPipeline.TelemetryMetrics do
     InstrumentationTracker.upsert(metric)
   end
 
-  defp track_message_instrumentation(measurements, metadata) do
-    # IO.inspect(metadata, label: "\ntelemetry_metrics track_message_instrumentation:\t")
-    %Message{} = message = metadata[:message]
-    # IO.inspect(message, label: "\nMessage:\t")
-    batcher = Map.get(message, :batcher)
-    partition = Map.get(message, :processor_key)
-    size = 1
-    metric_map =
-      %{
-        name: batcher,
-        partition: to_string(partition),
-        call_count: 1,
-        msg_count: size,
-        last_duration: measurements[:duration],
-        min_duration: measurements[:duration],
-        max_duration: measurements[:duration],
-        mean_duration: measurements[:duration],
-        first_time: measurements[:time],
-        last_time: measurements[:time]
-      }
-    metric = NodeMetric.new(metric_map)
-    # IO.inspect(metric, label: "\ntelemetry_metrics message metric:\t")
-    # InstrumentationTracker.upsert(metric)
-
-  end
-
   defp track_batcher_instrumentation(measurements, metadata) do
     # IO.inspect(metadata, label: "\ntelemetry_metrics track_batcher_instrumentation:\t")
 
@@ -146,47 +120,6 @@ defmodule TelemetryPipeline.TelemetryMetrics do
     metric = NodeMetric.new(metric_map)
     # IO.inspect(metric, label: "\ntelemetry_metrics batcher metric:\t")
     InstrumentationTracker.upsert(metric)
-  end
-
-  defp track_instrumentation(measurements, metadata) do
-    IO.inspect(measurements, label: "\ntelemetry_metrics measurements:\t")
-    IO.inspect(metadata, label: "\ntelemetry_metrics metadata:\t")
-
-    name_list =
-      metadata[:name]
-      |> IO.inspect(label: "\nname:\t")
-      |> to_string()
-      |> IO.inspect(label: "\nname string:\t")
-      |> String.split(~r{\.})
-      |> IO.inspect(label: "\nsplit:\t")
-      |> List.last()
-      |> IO.inspect(label: "\nlast:\t")
-      |> String.split("_")
-      |> IO.inspect(label: "\nname_list:\t")
-
-    [_node|tail] = name_list
-    [name|tail] = tail
-    [partition|_tail] = tail
-
-    IO.inspect(name, label: "\nname:\t")
-    IO.inspect(partition, label: "\npartition:\t")
-
-    # metric_map =
-      # %{
-        # name: name,
-        # partition: String.to_integer(partition),
-        # call_count: 1,
-        # msg_count: 1,
-        # last_duration: measurements[:duration],
-        # min_duration: measurements[:duration],
-        # max_duration: measurements[:duration],
-        # mean_duration: measurements[:duration],
-        # first_time: measurements[:time],
-        # last_time: measurements[:time]
-      # }
-    # metric = NodeMetric.new(metric_map)
-    # IO.inspect(metric, label: "\ntelemetry_metrics metric:\t")
-    # InstrumentationTracker.upsert(metric)
   end
 
 end
