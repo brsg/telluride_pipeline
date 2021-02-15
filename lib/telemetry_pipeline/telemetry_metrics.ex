fe2189c0b65971d9ed2e2c6a0d09b735cfb122f6
Alan Strait <alan@brsg.biz>
2021-02-14 19:21:01 -0700

    Track batcher metric.

--- lib/telemetry_pipeline/telemetry_metrics.ex
+++ lib/telemetry_pipeline/telemetry_metrics.ex
@@ -15,10 +15,30 @@ defmodule TelemetryPipeline.TelemetryMetrics do
   def handle_event([:broadway, :batcher, :start], _measurements, _metadata, _config) do
     # Logger.info("processor_name #{name} [:broadway, :batcher, :start] measurement #{inspect measurements} metadata #{inspect metadata}")
   end
-  def handle_event([:broadway, :batcher, :stop] = _msg, _measurements, _metadata, _config) do
-    # IO.inspect(msg, label: "\nhandle_event msg:\t")
-    # track_instrumentation( measurements, metadata)
-  end
+  def handle_event([:broadway, :batcher, :stop] = _msg, measurements, metadata, _config) do
+    name =
+      metadata[:name]
+      |> to_string()
+      |> String.split(~r{\.})
+      |> List.last()
+      |> String.replace_prefix("Batcher_", "")
+
+    metric_map =
+      %{
+        name: name,
+        partition: 0,
+        call_count: 1,
+        msg_count: 1,
+        last_duration: measurements[:duration],
+        min_duration: measurements[:duration],
+        max_duration: measurements[:duration],
+        mean_duration: measurements[:duration],
+        first_time: measurements[:time],
+        last_time: measurements[:time]
+      }
+    metric = NodeMetric.new(metric_map)
+    InstrumentationTracker.upsert(metric)
+   end
   def handle_event([:broadway, :consumer, :start], _measurements, _metadata, _config) do
     # Logger.info("name #{name} [:broadway, :consumer, :start] measurement #{inspect measurements} metadata #{inspect metadata}")
   end
@@ -88,40 +108,42 @@ defmodule TelemetryPipeline.TelemetryMetrics do
   end
 
   defp track_instrumentation(measurements, metadata) do
-    # IO.inspect(measurements, label: "\ntelemetry_metrics measurements:\t")
-    # IO.inspect(metadata, label: "\ntelemetry_metrics metadata:\t")
+    IO.inspect(measurements, label: "\ntelemetry_metrics measurements:\t")
+    IO.inspect(metadata, label: "\ntelemetry_metrics metadata:\t")
 
     name_list =
       metadata[:name]
-      # |> IO.inspect(label: "\nname:\t")
+      |> IO.inspect(label: "\nname:\t")
       |> to_string()
-      # |> IO.inspect(label: "\nname string:\t")
+      |> IO.inspect(label: "\nname string:\t")
       |> String.split(~r{\.})
-      # |> IO.inspect(label: "\nsplit:\t")
+      |> IO.inspect(label: "\nsplit:\t")
       |> List.last()
-      # |> IO.inspect(label: "\nlast:\t")
+      |> IO.inspect(label: "\nlast:\t")
       |> String.split("_")
-      # |> IO.inspect(label: "\nname_list:\t")
+      |> IO.inspect(label: "\nname_list:\t")
 
     [_node|tail] = name_list
     [name|tail] = tail
     [partition|_tail] = tail
 
+    IO.inspect(name, label: "\nname:\t")
+    IO.inspect(partition, label: "\npartition:\t")
 
-    metric_map =
-      %{
-        name: name,
-        partition: String.to_integer(partition),
-        call_count: 1,
-        msg_count: 1,
-        last_duration: measurements[:duration],
-        min_duration: measurements[:duration],
-        max_duration: measurements[:duration],
-        mean_duration: measurements[:duration],
-        first_time: measurements[:time],
-        last_time: measurements[:time]
-      }
-    metric = NodeMetric.new(metric_map)
+    # metric_map =
+      # %{
+        # name: name,
+        # partition: String.to_integer(partition),
+        # call_count: 1,
+        # msg_count: 1,
+        # last_duration: measurements[:duration],
+        # min_duration: measurements[:duration],
+        # max_duration: measurements[:duration],
+        # mean_duration: measurements[:duration],
+        # first_time: measurements[:time],
+        # last_time: measurements[:time]
+      # }
+    # metric = NodeMetric.new(metric_map)
     # IO.inspect(metric, label: "\ntelemetry_metrics metric:\t")
     # InstrumentationTracker.upsert(metric)
   end
